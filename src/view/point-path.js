c2dd1944f5fd23156e00b874b13d7e9d6d3a5144
ilya-Romankov <35827322+ilya-Romankov@users.noreply.github.com>
2022-01-26 15:25:51 +0700

    проверил eslint

--- src/view/point-path.js
+++ src/view/point-path.js
@@ -32,7 +32,7 @@ const createPointPathTemplate = (point) => {
       <div class="event__type">
         <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">${type} ${destination.name}</h3>
+      <h3 class="event__title">${type} ${he.encode(destination.name)}</h3>
       <div class="event__schedule">
         <p class="event__time">
           <time class="event__start-time" dateTime="2019-03-18T10:30">${dateFromHours}</time>
